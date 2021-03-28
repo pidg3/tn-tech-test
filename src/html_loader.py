9918e135aa5d78d6565e46982a5ad285c7bf2879
Michael Pidgeon <michael.r.pidgeon@gmail.com>
2021-03-28 16:33:06 +0100

    Fix: close down drivers when error

--- src/html_loader.py
+++ src/html_loader.py
@@ -13,27 +13,39 @@ def get_complete_page(url, await_data=[]):
   options.headless = True
   driver = webdriver.Firefox(options=options)
 
-  # Get the page (initially not fully loaded due to dynamic content)
-  driver.get(url)
+  # Wrap in a try/except so we can close down the driver if time out
+  try:
+    
+    # Get the page (initially not fully loaded due to dynamic content)
+    driver.get(url)
 
-  # Wait until dynamic content has loaded
-  for section in await_data:
-    WebDriverWait(driver, 5).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, f'[data-plugin-in-point-id={section}]')))
+    # Wait until dynamic content has loaded
+    for section in await_data:
+      WebDriverWait(driver, 5).until(
+          EC.presence_of_element_located((By.CSS_SELECTOR, f'[data-plugin-in-point-id={section}]')))
+
+    # Clear cookie warning (otherwise this gets in the way of us opening amenity list)
+    driver \
+      .find_element_by_css_selector("[data-testid=main-cookies-banner-container]") \
+      .find_element_by_class_name('_1qnlffd6') \
+      .click()
+
+    # Click through to get full amenity list modal
+    driver \
+      .find_element_by_class_name('_13e0raay') \
+      .click()
+
+    # Wait until amenities modal loaded
+    WebDriverWait(driver, 3).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, f'[data-testid=modal-container]')))
+
+    
+    # Return html and close webdriver connection
+    html = driver.page_source
+    driver.close()
+    return html
   
-  # Clear cookie warning (otherwise this gets in the way of us opening amenity list)
-  driver \
-    .find_element_by_css_selector("[data-testid=main-cookies-banner-container]") \
-    .find_element_by_class_name('_1qnlffd6') \
-    .click()
-
-  # Click through to get full amenity list modal
-  driver \
-    .find_element_by_class_name('_13e0raay') \
-    .click()
-
-  # Return html and close webdriver connection
-  html = driver.page_source
-  driver.close()
-
-  return html
+  # If anything goes wrong, close down the driver then re-throw
+  except Exception as e:
+    driver.close()
+    raise e
